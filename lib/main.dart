98cfd8b93dad606cb5ea0d9f54d117b17976c263
s3dibrahim <saad.ibrahim@pakdata.com>
2019-11-06 12:01:57 +0500

    Splash screen for Android updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:Quizit/routes/home.dart';
 import 'package:flutter/material.dart';
-import 'routes/splash.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: SplashScreen(),
+      home: HomePage(),
     );
   }
 }
